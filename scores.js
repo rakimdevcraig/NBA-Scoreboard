fabb758c57105135c41d910add4ff224764e60fb
rakimdevcraig <rakimdevcraig@gmail.com>
2020-01-09 23:27:11 -0500

    cleaned up some code

--- scores.js
+++ scores.js
@@ -10,28 +10,22 @@ axios.get(url)
     const html = response.data;
     let array = []
     let games = html.dates[0].games
-    for(let i = 0; i < games.length; i++){
-        let visitor = (games[i].vTeam.triCode)
-        let home = (games[i].hTeam.triCode)
-        let visitorScore = (games[i].vTeam.score)
-        let homeScore = (games[i].hTeam.score)
+    for (let i = 0; i < games.length; i++) {
+      let visitor = (games[i].vTeam.triCode)
+      let home = (games[i].hTeam.triCode)
+      let visitorScore = (games[i].vTeam.score)
+      let homeScore = (games[i].hTeam.score)
 
-        if(process.argv[2] === visitor || home === process.argv[2]){
-          
-          console.log(`Yes ${process.argv[2]} played tonight the score is:  ${home}   ${homeScore}  ${visitor} ${visitorScore}`)
+      if (process.argv[2] === visitor || home === process.argv[2]) {
 
-        }if(!process.argv[2]){
-          console.log(` ${home}  ${homeScore}  ${visitor} ${visitorScore}`)
-        }
+        console.log(`Yes ${process.argv[2]} played tonight the score is:  ${home}   ${homeScore}  ${visitor} ${visitorScore}`)
+
+      } if (!process.argv[2]) {
+        console.log(` ${home}  ${homeScore}  ${visitor} ${visitorScore}`)
+      }
 
     }
 
-    let vTeam = html.dates[1].games[0].vTeam.triCode
-    let vScore = html.dates[1].games[0].vTeam.score
-    let hTeam = html.dates[1].games[0].hTeam.triCode
-    let hScore = html.dates[1].games[0].hTeam.score
-
-    // console.log(html.dates[1])
   })
   .catch(function (error) {
     // handle error
